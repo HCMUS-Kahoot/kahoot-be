3755836e1a3114de304ee6791e534d377875c015
hatieudao <froyal2kk1@gmail.com>
2022-11-15 14:46:59 +0700

    feat: connect DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,24 @@
-import { ConfigModule } from '@nestjs/config';
+import { MONGO_CONNECTION_STRING } from './constant/index';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import configuration from './config/configuration';
 import { LoggerMiddleware } from './utils/logger.middleware';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env'],
       load: configuration,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>(MONGO_CONNECTION_STRING),
+      }),
     }),
   ],
   controllers: [AppController],
