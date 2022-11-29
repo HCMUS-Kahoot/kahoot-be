79ecd192eec240f6b63e457e8f4abd137c69b845
trandactoan <trandactoan2001@gmail.com>
2022-11-29 20:46:53 +0700

    google authentication

---
+++ src/modules/authentication/users/dto/update-user.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'email', type: String })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'password', type: String })
+  password: string;
+
+  @IsString()
+  @ApiProperty({ description: 'refreshToken', type: String })
+  refreshToken: string;
+}
