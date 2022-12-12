b4f13d275040cc53ed3c9ece0b97cee4545b2fa4
trandactoan <trandactoan2001@gmail.com>
2022-12-13 00:08:10 +0700

    add current-user-presentations

--- src/modules/presentations/presentations.controller.ts
+++ src/modules/presentations/presentations.controller.ts
@@ -1,9 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
 import { CreatePresentationDto } from "./dto/create-presentation.dto";
 import { UpdatePresentationDto } from "./dto/update-presentation.dto";
 import { PresentationDocument } from "./schema/presentations.schema";
 import { ApiTags } from '@nestjs/swagger';
 import { FactoryBaseController } from "../../base/factory-base.controller";
+import { PresentationsService } from "./presentations.service";
+import { JwtAuthGuard } from "../../common/guards/jwt-auth.guard";
+import { GetCurrentUserId } from "../../common/decorators/get-current-user-id.decorator";
 
 @Controller('presentations')
 @ApiTags('presentations')
@@ -12,5 +15,17 @@ export class PresentationsController extends FactoryBaseController<
   CreatePresentationDto,
   UpdatePresentationDto
 >(CreatePresentationDto, UpdatePresentationDto) {
-
+  constructor(
+    private readonly presentationsService: PresentationsService,
+  ) {
+    super(presentationsService);
+  }
+  @Get('current-user-presentations')
+  @UseGuards(JwtAuthGuard)
+  async getCurrentUserGroups(@GetCurrentUserId() id: string) {
+    const presentations = await this.presentationsService.getAll({ owner: id });
+    return Promise.all(presentations).then((result) => {
+      return result;
+    });
+  }
 }
