6b370fc64232fdbb40c9e36d1ea2696541e1e17e
trandactoan <trandactoan2001@gmail.com>
2023-01-09 21:53:39 +0700

    Add new collaborator

--- src/modules/presentations/presentations.service.ts
+++ src/modules/presentations/presentations.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import mongoose, { Model, mongo } from "mongoose";
 import { BaseService } from "../../base/base.service";
+import { User } from "../authentication/users/schema/users.schema";
+import { UsersService } from "../authentication/users/users.service";
 import { SlidesService } from "../slides/slides.service";
 import { PresentationsRepository } from "./presentations.repository";
 import { Presentation, PresentationDocument } from "./schema/presentations.schema";
-
 @Injectable()
 export class PresentationsService extends BaseService<PresentationDocument> {
   constructor(
@@ -13,10 +14,11 @@ export class PresentationsService extends BaseService<PresentationDocument> {
     private readonly presentationModel: Model<PresentationDocument>,
     private readonly presentationsRepository: PresentationsRepository,
     private readonly slideService: SlidesService,
+    private readonly usersService: UsersService,
     ) {
     super(presentationsRepository);
   }
-  async deleteAPresentation(presentationId: any): Promise<PresentationDocument>
+  async deleteAPresentation(presentationId: string): Promise<PresentationDocument>
   {
     const presentation = await this.presentationModel.findById(presentationId);
     if(presentation)
@@ -25,5 +27,21 @@ export class PresentationsService extends BaseService<PresentationDocument> {
       return await  this.presentationModel.findByIdAndDelete(presentationId);
     }
     throw NotFoundException;
-  }  
+  }
+  async addNewCollaborator(ownerId: string, presentationId: string, newCollaborator: User): Promise<User[]>
+  {
+    let presentation = await this.presentationModel.findOne({_id: presentationId, owner: ownerId});
+    if(presentation && newCollaborator)
+    {
+      if(!presentation.collaborators)
+      {
+        console.log("This is triggered when the collaborators is empty");
+        presentation.collaborators=new Array<User>();
+      }
+      if(presentation.collaborators.includes(newCollaborator)) return presentation.collaborators;
+      presentation.collaborators.push(newCollaborator);
+      return await (await presentation.save()).collaborators;
+    }
+    throw NotFoundException;
+  }
 }
