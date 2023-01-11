e4c94e485c3a4b4bce1e3fc5886723d895ef0c5b
hatieudao <froyal2kk1@gmail.com>
2023-01-11 12:29:55 +0700

    feat: filter

--- src/modules/presentations/presentations.controller.ts
+++ src/modules/presentations/presentations.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, Param, Post, Put, UseGuards } from "@nestjs/common";
-import { CreatePresentationDto } from "./dto/create-presentation.dto";
-import { UpdatePresentationDto } from "./dto/update-presentation.dto";
-import { PresentationDocument } from "./schema/presentations.schema";
+import { Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { CreatePresentationDto } from './dto/create-presentation.dto';
+import { UpdatePresentationDto } from './dto/update-presentation.dto';
+import { PresentationDocument } from './schema/presentations.schema';
 import { ApiTags } from '@nestjs/swagger';
-import { FactoryBaseController } from "../../base/factory-base.controller";
-import { PresentationsService } from "./presentations.service";
-import { JwtAuthGuard } from "../../common/guards/jwt-auth.guard";
-import { GetCurrentUserId } from "../../common/decorators/get-current-user-id.decorator";
-import { Override } from "../../common/decorators/override.decorator";
-import { Body, Delete } from "@nestjs/common/decorators";
-import { UsersService } from "../authentication/users/users.service";
+import { FactoryBaseController } from '../../base/factory-base.controller';
+import { PresentationsService } from './presentations.service';
+import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
+import { GetCurrentUserId } from '../../common/decorators/get-current-user-id.decorator';
+import { Override } from '../../common/decorators/override.decorator';
+import { Body, Delete } from '@nestjs/common/decorators';
+import { UsersService } from '../authentication/users/users.service';
+import { use } from 'passport';
 
 @Controller('presentations')
 @ApiTags('presentations')
@@ -24,48 +25,76 @@ export class PresentationsController extends FactoryBaseController<
   ) {
     super(presentationsService);
   }
-  @Get('current-user-presentations')
+  @Get('current-user-presentations/:email')
   @UseGuards(JwtAuthGuard)
-  async getCurrentUserPresentations(@GetCurrentUserId() id: string) {
-    const presentations = await this.presentationsService.getAll({ owner: id });
-    return Promise.all(presentations).then((result) => {
-      return result;
+  async getCurrentUserPresentations(
+    @GetCurrentUserId() id: string,
+    @Param('email') email: string,
+  ) {
+    const presentations = await this.presentationsService.getAll({
+      $or: [
+        { owner: id },
+        {
+          'collaborators.email': email,
+        },
+      ],
     });
+    return Promise.all(presentations).then((result) => result);
   }
-
   @Get('current-user-presentations-group/:groupId')
   @UseGuards(JwtAuthGuard)
-  async getCurrentUserPresentationsGroup(@GetCurrentUserId() id: string, @Param('groupId') groupId: string)
-  {
-    const presentations = await this.presentationsService.getAll({ owner: id, groupId})
+  async getCurrentUserPresentationsGroup(
+    @GetCurrentUserId() id: string,
+    @Param('groupId') groupId: string,
+  ) {
+    const presentations = await this.presentationsService.getAll({
+      owner: id,
+      groupId,
+    });
     return Promise.all(presentations).then((result) => {
       return result;
     });
   }
 
   @Post('/name/:id/:name')
-  async updatePresentationName(@Param('id') id: string, @Param('name') name: string) {
+  async updatePresentationName(
+    @Param('id') id: string,
+    @Param('name') name: string,
+  ) {
     return this.presentationsService.updateOne(id, {
-      name
+      name,
     });
   }
   @Post('addCollaborator')
-  async addCollaborator(@Body() body: any)
-  {
-    const {ownerId, presentationId, collaboratorEmail} = body;
-    const newCollaborator = await this.usersService.getUserByEmail(collaboratorEmail);
-    return await this.presentationsService.addNewCollaborator(ownerId, presentationId, newCollaborator);
+  async addCollaborator(@Body() body: any) {
+    const { ownerId, presentationId, collaboratorEmail } = body;
+    const newCollaborator = await this.usersService.getUserByEmail(
+      collaboratorEmail,
+    );
+    return await this.presentationsService.addNewCollaborator(
+      ownerId,
+      presentationId,
+      newCollaborator,
+    );
   }
   @Post('deleteCollaborator')
-  async deleteCollaborator(@Body() body: any)
-  {
-    const {ownerId, presentationId, collaboratorEmail} = body;
-    const deleteCollaborator = await this.usersService.getUserByEmail(collaboratorEmail);
-    return await this.presentationsService.deleteCollaborator(ownerId, presentationId, deleteCollaborator);
+  async deleteCollaborator(@Body() body: any) {
+    const { ownerId, presentationId, collaboratorEmail } = body;
+    const deleteCollaborator = await this.usersService.getUserByEmail(
+      collaboratorEmail,
+    );
+    return await this.presentationsService.deleteCollaborator(
+      ownerId,
+      presentationId,
+      deleteCollaborator,
+    );
   }
-  
+
   @Override()
-  async delete(id: string, option?: any): Promise<Partial<PresentationDocument>> {
-      return await this.presentationsService.deleteAPresentation(id);
+  async delete(
+    id: string,
+    option?: any,
+  ): Promise<Partial<PresentationDocument>> {
+    return await this.presentationsService.deleteAPresentation(id);
   }
 }
--- src/modules/presentations/presentations.service.ts
+++ src/modules/presentations/presentations.service.ts
@@ -1,12 +1,15 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import mongoose, { Model, mongo } from "mongoose";
-import { BaseService } from "../../base/base.service";
-import { User } from "../authentication/users/schema/users.schema";
-import { UsersService } from "../authentication/users/users.service";
-import { SlidesService } from "../slides/slides.service";
-import { PresentationsRepository } from "./presentations.repository";
-import { Presentation, PresentationDocument } from "./schema/presentations.schema";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import mongoose, { Model, mongo } from 'mongoose';
+import { BaseService } from '../../base/base.service';
+import { User } from '../authentication/users/schema/users.schema';
+import { UsersService } from '../authentication/users/users.service';
+import { SlidesService } from '../slides/slides.service';
+import { PresentationsRepository } from './presentations.repository';
+import {
+  Presentation,
+  PresentationDocument,
+} from './schema/presentations.schema';
 @Injectable()
 export class PresentationsService extends BaseService<PresentationDocument> {
   constructor(
@@ -15,41 +18,61 @@ export class PresentationsService extends BaseService<PresentationDocument> {
     private readonly presentationsRepository: PresentationsRepository,
     private readonly slideService: SlidesService,
     private readonly usersService: UsersService,
-    ) {
+  ) {
     super(presentationsRepository);
   }
-  async deleteAPresentation(presentationId: string): Promise<PresentationDocument>
-  {
+  async deleteAPresentation(
+    presentationId: string,
+  ): Promise<PresentationDocument> {
     const presentation = await this.presentationModel.findById(presentationId);
-    if(presentation)
-    {
-      await this.slideService.deleteSlidesByPresentationId(presentationId)
-      return await  this.presentationModel.findByIdAndDelete(presentationId);
+    if (presentation) {
+      await this.slideService.deleteSlidesByPresentationId(presentationId);
+      return await this.presentationModel.findByIdAndDelete(presentationId);
     }
     throw NotFoundException;
   }
-  async addNewCollaborator(ownerId: string, presentationId: string, newCollaborator: User): Promise<User[]>
-  {
-    let presentation = await this.presentationModel.findOne({_id: presentationId, owner: ownerId});
-    if(presentation && newCollaborator)
-    {
-      if(!presentation.collaborators)
-      {
-        presentation.collaborators=new Array<User>();
+  async addNewCollaborator(
+    ownerId: string,
+    presentationId: string,
+    newCollaborator: User,
+  ): Promise<User[]> {
+    let presentation = await this.presentationModel.findOne({
+      _id: presentationId,
+      owner: ownerId,
+    });
+    if (presentation && newCollaborator) {
+      if (!presentation.collaborators) {
+        presentation.collaborators = new Array<User>();
       }
-      if(presentation.collaborators.includes(newCollaborator)) return presentation.collaborators;
-      await this.presentationModel.findByIdAndUpdate(presentationId,{collaborators: [...presentation.collaborators, newCollaborator]})
-      return await (await this.presentationModel.findById(presentationId)).collaborators;
+      if (presentation.collaborators.includes(newCollaborator))
+        return presentation.collaborators;
+      await this.presentationModel.findByIdAndUpdate(presentationId, {
+        collaborators: [...presentation.collaborators, newCollaborator],
+      });
+      return await (
+        await this.presentationModel.findById(presentationId)
+      ).collaborators;
     }
     throw NotFoundException;
   }
-  async deleteCollaborator(ownerId: string, presentationId: string, deleteCollaborator: User): Promise<User[]>
-  {
-    let presentation = await this.presentationModel.findOne({_id: presentationId, owner: ownerId});
-    if(presentation && deleteCollaborator)
-    {
-      await this.presentationModel.findByIdAndUpdate(presentationId,{collaborators: presentation.collaborators.filter(user => user.email !== deleteCollaborator.email)})
-      return await (await this.presentationModel.findById(presentationId)).collaborators;
+  async deleteCollaborator(
+    ownerId: string,
+    presentationId: string,
+    deleteCollaborator: User,
+  ): Promise<User[]> {
+    let presentation = await this.presentationModel.findOne({
+      _id: presentationId,
+      owner: ownerId,
+    });
+    if (presentation && deleteCollaborator) {
+      await this.presentationModel.findByIdAndUpdate(presentationId, {
+        collaborators: presentation.collaborators.filter(
+          (user) => user.email !== deleteCollaborator.email,
+        ),
+      });
+      return await (
+        await this.presentationModel.findById(presentationId)
+      ).collaborators;
     }
     throw NotFoundException;
   }
